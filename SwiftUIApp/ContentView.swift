12805b1422a2045a33f5165a775719332631de0a
shamil92 <kammaev@mail.ru>
2021-12-17 10:46:32 +0300

    Initial Commit

---
+++ SwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIApp
+//
+//  Created by Шамиль Каммаев on 17.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
